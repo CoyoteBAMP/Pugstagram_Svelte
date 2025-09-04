168319cb1585e7c82ee12c3b946d94794e7e289e
CoyoteBAMP <101156019+CoyoteBAMP@users.noreply.github.com>
2025-09-03 20:31:01 -0600

    Add files via upload

---
+++ src/components/Share.svelte
@@ -0,0 +1,60 @@
+<style>
+  .Share {
+    border: 1px solid rgba(219, 219, 219, 1);
+    border-radius: 4px;
+    background-color: white;
+    margin: 0 0 2em 0;
+    width: 300px;
+    height: 80px;
+    position: absolute;
+    top: 300px;
+    left: 30%;
+    z-index: 9;
+    padding: 1em;
+  }
+  .Share-head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .Share-head h2 {
+    font-size: 16px;
+  }
+  .icon-button {
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .Share-content a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: black;
+    font-size: 14px;
+    margin: 2em 0 0 0;
+  }
+  .Share-content i {
+    color: #3b5998;
+    margin: 0 0.5em 0 0;
+    font-size: 20px;
+  }
+</style>
+
+<div class="Share">
+  <div class="Share-head">
+    <h2>Compartir</h2>
+    <button class="icon-button" on:click>
+      <i class="fas fa-times-circle"></i>
+    </button>
+  </div>
+  <div class="Share-content">
+    <a href="https://www.facebook.com/sharer.php?&u=https://pugstagram.com" target="_blank">
+      <i class="fab fa-facebook-square"></i>
+      Compartir en Facebook
+    </a>
+  </div>
+</div>
---
+++ src/containers/App.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { onMount } from 'svelte';
+    import Header from '../components/Header.svelte';
+    import Main from '../components/Main.svelte';
+    import Sidebar from '../components/Sidebar.svelte';
+    import Timeline from '../components/Timeline.svelte';
+
+    let data = {};
+    const API = 'https://kittygram-api-lg7jjtvfh.now.sh/';
+
+    onMount(async () => {
+        const response = await fetch(API);
+        const result = await response.json();
+
+        // Filtramos posts que tengan username no deseado
+        const blockedUsernames = ["Thurston Waffles", "Grumpy"];
+        const filteredPosts = result.posts.filter(post => !blockedUsernames.includes(post.username));
+
+        // Guardamos en data los posts filtrados
+        data = { ...result, posts: filteredPosts };
+    });
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+    
+    :global(body) {
+        background-color: #fafafa;
+        color: rgba(38, 38, 38, 0.7);
+        font-family: "Lato", sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+    :global(h1, h2, h3){
+        margin: 0;
+        padding: 0;
+    }
+</style>
+
+<Header />
+<Main>
+    <Timeline posts={data.posts}/>
+    <Sidebar {...data.user}/>
+</Main>
